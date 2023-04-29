b2758a65b90f9424a9bdc1a2c627aa889cc96a21
cuatrotales <jfquinterogu14@gmail.com>
2023-04-29 11:35:38 -0500

    se crea escuderia

---
+++ Escuderia.py
@@ -0,0 +1,44 @@
+from Piloto import Piloto
+
+class Escuderia:
+    def __init__(self):
+        self.__nombre =None
+        self.__casamotor = None
+        self.__pilotoPrincipal = None
+        self.__pilotosSecundario = None
+
+@property
+def nombre(self):
+    return self.__nombre
+
+@nombre.setter
+def nombre(self,dato):
+        self.__nombre = dato
+
+@property
+def casamotor(self):
+    return self.__casamotor
+
+@casamotor.setter
+def casamotor(self,dato):
+        self.__casamotor = dato
+
+
+@property
+def pilotoPrincipal(self):
+    return self.__pilotoPrincipal
+
+@pilotoPrincipal.setter
+def casamotor(self,dato):
+        self.__pilotoPrincipal = dato
+
+@property
+def pilotosSecundario(self):
+    return self.__pilotosSecundario
+
+@pilotosSecundario.setter
+def pilotosSecundario(self,dato):
+        self.__pilotosSecundario = dato
+
+
+
