c63a0bc9bebf9126b8f1859566fe210a6da16c6e
wilyCesde <wruizza@cesde.net>
2023-04-29 11:36:30 -0500

    un commit

---
+++ Escuderia.py
@@ -0,0 +1,51 @@
+
+from Piloto import Piloto
+
+class Escuderia:
+    def __init__(self):
+        self.__nombre= None
+        self.__casaMotor= None
+        self.__pilotoPrincipal= Piloto()
+        self.__pilotoSegundario= Piloto()
+        self.__costos= None
+        
+@property
+def nombre(self):
+ return self.__nombre
+
+ @nombre.setter 
+ def nombre(self, dato):
+    self.__nombre = dato
+
+@property
+def __casaMotor(self):
+ return self.__casaMotor
+
+ @casaMotor.setter 
+ def nombre(self, dato):
+    self.__casaMotor = dato
+    
+@property
+def __pilotoPrincipal(self):
+ return self.__pilotoPrincipal
+
+ @casaMotor.setter 
+ def pilotoPrincipal(self, dato):
+    self.__pilotoPrincipal = dato
+
+@property
+def __pilotoSegundario(self):
+ return self.__pilotoSegundario
+
+ @casaMotor.setter 
+ def pilotoSegundario(self, dato):
+    self.__pilotoSegundario = dato
+       
+@property
+def __costos(self):
+ return self.__costos
+
+ @casaMotor.setter 
+ def costos(self, dato):
+    self.__costos = dato
+       
--- Piloto.py
+++ Piloto.py
@@ -5,3 +5,35 @@ class Piloto:
         self.__salarioAnual= None
         self.__Pais= None
         
+@property
+def nombre(self):
+ return self.__nombre
+
+ @nombre.setter 
+ def nombre(self, dato):
+    self.__nombre = dato
+    
+@property
+def __fechaNacimiento(self):
+ return self.__fechaNacimiento
+
+ @fechaNacimiento.setter 
+ def fechaNacimiento(self, dato):
+    self.__fechaNacimiento = dato
+    
+@property
+def __salarioAnual(self):
+ return self.__salarioAnual
+
+ @__salarioAnual.setter 
+ def nombre(self, dato):
+    self.__salarioAnual = dato
+    
+@property
+def __Pais(self):
+ return self.__Pais
+
+ @Pais.setter 
+ def __Pais(self, dato):
+    self.__salarioAnual = dato
+    
