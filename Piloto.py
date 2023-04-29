6d64119e9f319e9edff6996c7f005cb4d65a599d
wilyCesde <wruizza@cesde.net>
2023-04-29 11:10:39 -0500

    resivo cambios

--- Piloto.py
+++ Piloto.py
@@ -1,9 +1,3 @@
-class Piloto :
-   def  __init__(self, ):
-      self.__nombre=None
-      self.__edad =None
-      self.__pais =None
-      self.__pais =None
 class Piloto:
     def __init__(self):
         self.__nombre= None
