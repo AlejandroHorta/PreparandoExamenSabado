01b9e7f8d7257b9430ac21a9b07e38fa30ab5e2f
Steven Torres Calero <stclink@gmail.com>
2023-04-29 12:02:22 -0500

    NT11 se crea main con lógica

---
+++ main.py
@@ -0,0 +1,20 @@
+from Escuderia import Escuderia
+
+escuderias = []
+contador = 1
+
+numeroEscuderias = int(input("¿Cuántas escuderías hay esta temporada? "))
+
+while contador < numeroEscuderias:
+    escuderia = Escuderia()
+    escuderia.nombre = input("Digite el nombre de la escudería. ")
+    escuderia.casaMotor = input("Digite el nombre de la casa motor. ")
+    escuderia.pilotoPrincipal.nombre = input("Digite el nombre del piloto principal. ")
+    escuderia.pilotoPrincipal.salarioAnual = input("Digite el salario del piloto principal. ")
+    escuderia.pilotoPrincipal.Pais = input("Digite el País] del piloto principal. ")
+    escuderia.piloto2.nombre = input("Digite el nombre del piloto 2. ")
+    escuderia.costos = input("Digite costos. ")
+
+    contador += 1
+
+escuderias.append(escuderia)
