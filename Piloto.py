dbd628103b77634e1bf0d64b9050b6d18345d423
Steven Torres Calero <stclink@gmail.com>
2023-04-29 11:35:26 -0500

    NT11 Se crean clases Escuderia y piloto

--- Piloto.py
+++ Piloto.py
@@ -1,11 +1,3 @@
-
-class Piloto:
-    
-    def __init__(self):
-        self.__nombre=None
-        self.__fechaNacimiento=None
-        self.__salarioAnual=None
-        self.__pais=None
 class Piloto:
     def __init__(self):
         self.__nombre= None
@@ -13,3 +5,30 @@ class Piloto:
         self.__salarioAnual= None
         self.__Pais= None
         
+@property
+def nombre(self):
+    return self.__nombre
+@property
+def fechaNacimiento(self):
+    return self.__fechaNacimiento
+@property
+def salarioAnual(self):
+    return self.__salarioAnual
+@property
+def Pais(self):
+    return self.__Pais
+
+
+@nombre.setter
+def nombre(self, dato):
+    self.__nombre=dato
+@nombre.setter
+def fechaNacimiento(self, dato):
+    self.__fechaNacimiento=dato
+@nombre.setter
+def fechaNacimiento(self, dato):
+    self.__salarioAnual=dato
+@nombre.setter
+def Pais(self, dato):
+    self.__Pais=dato
+    
